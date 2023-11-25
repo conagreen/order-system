923e8463c5627a4e500690b5390ad90990bcaeb9
conagreen <verycona@gmail.com>
2023-11-25 21:57:04 +0900

    feat: 주문 요청 시 상품 카테고리 선택할 수 있도록 구현

--- src/com/order/system/Main.java
+++ src/com/order/system/Main.java
@@ -1,15 +1,48 @@
 package com.order.system;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("주문하시겠습니까? (1: 주문, 2: 종료)");
-        process(scanner.nextInt());
+    Map<String , String> categories;
+
+    public Main() {
+        categories = new HashMap<>();
+        categories.put("1", "의류");
+        categories.put("2", "가방");
+        categories.put("3", "신발");
     }
 
-    private static void process(int answer) {
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.process();
+    }
 
+    private void process() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("주문하시겠습니까? (o: 주문, q: 종료)");
+        String answer = scanner.nextLine();
+
+        switch (answer) {
+            case "o" -> {
+                String categoryId = selectItemCategory();
+            }
+            case "q" -> System.out.println("주문을 종료합니다. 감사합니다.");
+            default -> {
+                System.out.println("올바른 값을 입력하십시오.");
+                process();
+            }
+        }
+    }
+
+    private String selectItemCategory() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("카테고리 번호를 입력하십시오. (q: 종료)");
+        for (Map.Entry<String, String> category : categories.entrySet()) {
+            System.out.println(category.getKey() + ". " + category.getValue());
+        }
+
+        return scanner.nextLine();
     }
 }
