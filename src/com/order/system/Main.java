278a38d8c55675f5c7851fd5c2e2ef38d3895a26
conagreen <verycona@gmail.com>
2023-11-25 20:03:18 +0900

    feat: '주문' 또는 '종료'를 선택할 수 있도록 입력 제공

--- src/com/order/system/Main.java
+++ src/com/order/system/Main.java
@@ -1,8 +1,15 @@
 package com.order.system;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("주문하시겠습니까? (1: 주문, 2: 종료)");
+        process(scanner.nextInt());
+    }
+
+    private static void process(int answer) {
+
     }
 }
